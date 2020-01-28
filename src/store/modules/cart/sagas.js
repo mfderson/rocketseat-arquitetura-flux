8b19d1beb765e9584db709ce640b04085823669d
Anderson Freitas <mfderson@gmail.com>
2020-01-27 22:17:22 -0300

    chore: Estoque na alteração

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -4,7 +4,7 @@ import { toast } from 'react-toastify';
 import api from '../../../services/api';
 import { formatPrice } from '../../../util/format';
 
-import { addToCartSuccess, updateAmount } from './actions';
+import { addToCartSuccess, updateAmountSuccess } from './actions';
 
 // Essa declaração de função com * quer dizer que vamos usar o generator, que é
 // uma funcionalidade do javascript
@@ -28,7 +28,7 @@ function* addToCart({ id }) {
   if (productExists) {
     const amount = productExists.amount + 1;
 
-    yield put(updateAmount(id, amount));
+    yield put(updateAmountSuccess(id, amount));
   } else {
     // O yield funciona como o await
     const response = yield call(api.get, `/products/${id}`);
@@ -44,6 +44,23 @@ function* addToCart({ id }) {
   }
 }
 
+function* updateAmount({ id, amount }) {
+  if (amount <= 0) return;
+
+  const stock = yield call(api.get, `stock/${id}`);
+  const stockAmount = stock.data.amount;
+
+  if (amount > stockAmount) {
+    toast.error('Quantidade solicitada fora de estoque');
+    return;
+  }
+
+  yield put(updateAmountSuccess(id, amount));
+}
+
 // Faz o bind de qual action quer ouvir para qual função quer disparar
 // O takeLatest inibe vários clicks do usuário até que o yield seja executado
-export default all([takeLatest('@cart/ADD_REQUEST', addToCart)]);
+export default all([
+  takeLatest('@cart/ADD_REQUEST', addToCart),
+  takeLatest('@cart/UPDATE_AMOUNT_REQUEST', updateAmount),
+]);
